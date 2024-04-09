4b27c0edf963af2e60d3304cb97a5e4d154e3cb8
Marat-Tim <rurikcat@gmail.com>
2024-04-09 21:58:30 +0300

    Added a console interface project for the program

---
+++ TestImpactAnalysis.Cli/Options.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+using TestImpactAnalysis.Coverage.Impl;
+using TestImpactAnalysis.Database;
+
+namespace TestImpactAnalysis.Cli;
+
+public class Options
+{
+    [Option('d', "testsdll", Required = true, HelpText = "Path to tests dll")]
+    public string PathToTestsDll { get; set; }
+    
+    [Option('t', "testsproj", Required = true, HelpText = "Path to directory with tests csproj file")]
+    public string PathToTestsProject { get; set; }
+
+    [Option('g', "git", Required = true, HelpText = "Path to directory with .git folder")]
+    public string PathToDirectoryWithGit { get; set; }
+    
+    [Option('b', "commit1", Required = true, HelpText = "First commit hash")]
+    public string Commit1Hash { get; set; }
+
+    [Option('a', "commit2", Required = true, HelpText = "Second commit hash")]
+    public string Commit2Hash { get; set; }
+    
+    [Option('c', "connection", Required = true, HelpText = "Connection string to database")]
+    public string DbConnection { get; set; }
+
+    [Option('s', "dbtype", Required = true, HelpText = "Type of database")]
+    public DatabaseType DbType { get; set; }
+    
+    [Option('v', "verbosity", Required = true, HelpText = "Logging detail level")]
+    public CmdTestRunner.OutputDetalization Verbosity { get; set; }
+}
---
+++ TestImpactAnalysis.Cli/Program.cs
@@ -0,0 +1,21 @@
+﻿using CommandLine;
+using TestImpactAnalysis.Cli;
+using TestImpactAnalysis.Default;
+
+Parser.Default.ParseArguments<Options>(args)
+    .WithParsed(options =>
+    {
+        var tests = new TestsThatDependsOnChanges(
+            options.PathToTestsDll,
+            options.PathToTestsProject,
+            options.PathToDirectoryWithGit,
+            options.Commit1Hash,
+            options.Commit2Hash,
+            options.DbConnection,
+            options.DbType,
+            options.Verbosity);
+        foreach (var test in tests)
+        {
+            Console.WriteLine(test);
+        }
+    });
